7e9f6e67a378a90345948930af8a3864f3d02ed6
Markus Schleifer <markus.schleifer@rarebyte.com>
2024-03-27 18:42:24 +0100

    Add Heatmap Config

--- scripts/generate_heat_map.py
+++ scripts/generate_heat_map.py
@@ -1,7 +1,6 @@
 # region Imports
 from datetime import datetime
 
-import cv2
 import pandas as pd
 import seaborn as sns
 import matplotlib.pylab as plt
@@ -9,10 +8,9 @@ import numpy as np
 from tqdm import tqdm
 
 from config.config import DATE_FORMAT, ASSETS_FOLDER, HEATMAPS_FOLDER
+from config.heatmap_config import MARKER_SIZE, OUTPUT_IMAGE_SIZE, BW_ADJUST, GRID_SIZE, LEVELS, ALPHA
 from db.fetching_db import fetch_telemetry_data, fetch_matches
-from helper.legend_line import LegendLine
 from helper.my_logger import logger
-from scripts.generate_histogram import prepare_legend_background, add_credits_to_legend
 
 # python3.7 (or lower?)
 # plt.style.use("seaborn")
@@ -41,24 +39,36 @@ def create_heat_maps(server, maps, date_string, due_date_string, zone):
 
         server_name = "E-SPORT" if server == "esport" else "LIVE" if server == "live" else "E-SPORT/LIVE"
         title = f"2023 {server_name} " + map_i[1] + " (# maps: " + str(len(circles)) + ")"
-        create_heat_map_for_end_circles(circles, map_i, server, title, date_string, due_date_string, zone, bw_adjust=0.5)
-        create_heat_map_for_end_circles(circles, map_i, server, title, date_string, due_date_string, zone, bw_adjust=0.33)
+
+        for bw_adjust in BW_ADJUST:
+            create_heat_map_for_end_circles(circles, map_i, server, title, date_string, due_date_string, zone, bw_adjust=bw_adjust)
+
 
 
 def create_heat_map_for_end_circles(circles, map_tuple, server, title, date, due_date, zone, bw_adjust=1.0):
+    # adjust dataframe values
     df = pd.DataFrame(columns=['x', 'y'], data=circles)
-
     df['x'] = df['x'] * 0.00001
     df['y'] = df['y'] * 0.00001
 
+    # hhh
+    resizer = OUTPUT_IMAGE_SIZE / 10
+    # map name
     map_name = map_tuple[0].lower()
     map_name_pretty = map_tuple[1].lower()
 
-    img = plt.imread(f"{ASSETS_FOLDER}/{map_name}.png")
-    fig, ax = plt.subplots(figsize=(16, 16))
+    # output file naming
+    date_pretty = str(date).replace("-", "_")
+    due_date_pretty = str(due_date).replace("-", "_")
+    bw_string = str(bw_adjust).replace(".", "")
+    result_folder = f"{HEATMAPS_FOLDER}/{server}/{map_name_pretty}"
+    plot_output_name = f"{map_name_pretty}_zone-{zone}_bw-{bw_string}_from-{date_pretty}_to-{due_date_pretty}"
 
+    # create figure
+    fig, ax = plt.subplots(figsize=(OUTPUT_IMAGE_SIZE, OUTPUT_IMAGE_SIZE))
+
+    # create legend
     ticks = np.arange(0.0, 9, 1.02)
-
     ax.yaxis.set_ticks(ticks)
     ax.xaxis.set_ticks(ticks)
     ax.grid(axis="x", alpha=0.5)
@@ -66,33 +76,35 @@ def create_heat_map_for_end_circles(circles, map_tuple, server, title, date, due
     ax.xaxis.tick_top()
     ax.set(ylim=(8.16, 0))
     ax.set(xlim=(0, 8.16))
-    ax.imshow(img, extent=[0, 8.16, 8.16, 0])
+
+    # write credits
     ax.text(0.5, 0.95, title, transform=ax.transAxes,
-            fontsize=26, color='white', alpha=.75,
+            fontsize=24 * resizer, color='white', alpha=.75,
             ha='center', va='top')
     ax.text(0.125, 0.16, '@Schleifinho', transform=ax.transAxes,
-            fontsize=26, color='white', alpha=0.25,
+            fontsize=24 * resizer, color='white', alpha=0.25,
             ha='left', va='top')
 
-    sns.kdeplot(data=df, x='x', y='y', fill=True, alpha=0.4, legend=True, levels=50, gridsize=400, ax=ax,
+    # load bg image of map
+    img = plt.imread(f"{ASSETS_FOLDER}/{map_name}.png")
+    ax.imshow(img, extent=[0, 8.16, 8.16, 0])
+
+    # plot data
+    sns.kdeplot(data=df, x='x', y='y', fill=True, alpha=ALPHA, legend=True, levels=LEVELS, gridsize=GRID_SIZE, ax=ax,
                 bw_adjust=bw_adjust, cmap='Spectral_r', cbar=True, warn_singular=False)  # turbo
 
-    date_pretty = str(date).replace("-", "_")
-    due_date_pretty = str(due_date).replace("-", "_")
-    bw_string = str(bw_adjust).replace(".", "")
-    folder_and_name = f"{HEATMAPS_FOLDER}/{server}/{map_name_pretty}/{map_name_pretty}"\
-                      f"_zone-{zone}_{bw_string}_from-{date_pretty}_to-{due_date_pretty}.jpg"
 
-    plt.savefig(folder_and_name)
+    full_name = f"{result_folder}/{plot_output_name}.jpg"
+    full_name_points = f"{result_folder}/{plot_output_name}_points.jpg"
+    plt.savefig(full_name)
 
-    sns.scatterplot(data=df, x='x', y='y', s=75, label="Zone Center", marker='o', edgecolor="white")
+    sns.scatterplot(data=df, x='x', y='y', s=MARKER_SIZE * resizer, label="Zone Center", marker='o', edgecolor="white")
 
-    lgnd = ax.legend(bbox_to_anchor=(1.35, 1))
-    lgnd.legendHandles[0].set_sizes([100])
+    lgnd = ax.legend(bbox_to_anchor=(1.35, 1), fontsize=8*resizer)
+    lgnd.legendHandles[0].set_sizes([100 * resizer])
 
-    folder_and_name = f"{HEATMAPS_FOLDER}/{server}/{map_name_pretty}/{map_name_pretty}" \
-                      f"_zone-{zone}_bw-{bw_string}_from-{date_pretty}_to-{due_date_pretty}_points.jpg"
-    plt.savefig(folder_and_name)
+    # plt.show()
+    plt.savefig(full_name_points)
 
 
 # endregion
